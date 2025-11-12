80d5b11ca42da7d8315eda938dfd61e84b04a7b8
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2025-11-12 10:37:40 -0800

    Add pull for 61b subdirectories

--- pull_all.sh
+++ pull_all.sh
@@ -1,5 +1,11 @@
-for dir in */; do
+for dir in ~/repos/*; do
   if [ -d "$dir/.git" ]; then
+    echo "Pulling updates in $dir..."
+    git -C "$dir" pull
+  fi
+done
+for dir in ~/repos/cs61b/*; do
+    if [ -d "$dir/.git" ]; then
     echo "Pulling updates in $dir..."
     git -C "$dir" pull
   fi
