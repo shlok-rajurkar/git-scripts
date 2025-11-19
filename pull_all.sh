1716fcb652fea044555ee42105af56e59d2d1da2
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2025-11-18 19:12:03 -0800

    Modify for linux file organization

--- pull_all.sh
+++ pull_all.sh
@@ -10,3 +10,15 @@ for dir in ~/repos/cs61b/*; do
     git -C "$dir" pull
   fi
 done
+for dir in ~/data/repos/*; do
+  if [ -d "$dir/.git" ]; then
+    echo "Pulling updates in $dir..."
+    git -C "$dir" pull
+  fi
+done
+for dir in ~/data/repos/cs61b/*; do
+    if [ -d "$dir/.git" ]; then
+    echo "Pulling updates in $dir..."
+    git -C "$dir" pull
+  fi
+done
